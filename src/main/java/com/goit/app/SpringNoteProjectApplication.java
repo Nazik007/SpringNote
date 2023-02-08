d63bc26615d8272532252d031f30655dfc93111b
Nazarii Soviak <natesoviak@gmail.com>
2023-02-08 13:03:15 +0200

    Initial commit

---
+++ src/main/java/com/goit/app/SpringNoteProjectApplication.java
@@ -0,0 +1,12 @@
+package com.goit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNoteProjectApplication.class, args);
+    }
+}
---
+++ src/main/java/com/goit/app/note/NoteService.java
@@ -0,0 +1,50 @@
+package com.goit.app.note;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@RequiredArgsConstructor
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    public List<Note> listAll() {
+        return noteRepository.findAll();
+    }
+
+    public Note add(Note note) {
+        noteRepository.save(note);
+        return note;
+    }
+
+    public void deleteById(long id) {
+        noteRepository.findById(id)
+                .stream()
+                .findAny()
+                .orElseThrow(() -> {
+                    throw new NoSuchElementException("The note is missing.");
+                });
+        noteRepository.deleteById(id);
+    }
+
+    public void update(Note note) {
+        noteRepository.findById(note.getId())
+                .stream()
+                .findAny()
+                .orElseThrow(() -> {
+                    throw new NoSuchElementException("The note is missing.");
+                });
+        noteRepository.save(note);
+    }
+
+    public Note getById(long id) {
+        return noteRepository.findById(id)
+                .stream()
+                .findAny()
+                .orElseThrow(() -> {
+                    throw new NoSuchElementException("The note is missing.");
+                });
+    }
+}
